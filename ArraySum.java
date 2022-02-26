d12ef26ea88c456c434882cc7022eb401f5af1fc
strikerus045 <63329537+strikerus045@users.noreply.github.com>
2022-02-25 20:58:39 -0500

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,13 +1,13 @@
 
-public class ArraySum {			
+public class ArraySum {			//method
 	public int sumOfArray(Integer[]a, int index) {
-		
+		//base case
 		if (index==0)
 		{	
 			return a[index];
 			}
 				else
-				{								
+				{		//method call back						
 					return a[index]+sumOfArray(a, index -1);
 					}
 	}
